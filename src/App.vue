ba88a4de2cdd051ed629d2ad39dc1bd53aa8382a
Dario Becchio <dabek1986@gmail.com>
2024-01-27 14:59:59 +0100

    Test salvataggio

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+  name:'Exercise',
+  data (){
+    return{
+    title:'Foto del mare',
+    color:'red',
+    image: 'https://www.focus.it/images/2021/11/23/mare_1020x680.jpg',
+    }
+    
+  }
+
+}
+</script>
+
+<template>
+  <h1 :class="color">{{ title }}</h1>
+  <img :src="image" alt="mare">
+</template>
+
+<style >
+
+</style>
